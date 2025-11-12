b14419d0aa283d8543a0362480b1be2d8abd321a
Emmanuel Olayinka <olayinkaemma27@gmail.com>
2025-11-12 14:49:56 +0100

    Remove Wordlinkapp folder

--- Wordlinkapp/src/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { logIn, signUp } from "../controllers/authController.js";
-
-const app = express.Router();
-
-app.post("/signup", signUp);
-app.post("/login", logIn);
-
-export default app;
